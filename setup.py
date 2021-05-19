cb308d338930e1cd9e25cf9a5147c062c5cc2daa
Marian Ignev <m.ignev@gmail.com>
2021-05-19 17:11:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='sklearn-json',
+     name='th-sklearn-json',
      version='0.1.0',
-     author="Mathieu Rodrigue",
-     author_email="support@mlrequest.com",
-     description="A safe, transparent way to share and deploy scikit-learn models.",
+     author="Marian Ignev",
+     author_email="dev@teachablehub.com",
+     description="A safe, transparent way to share and deploy scikit-learn models. Original by Mathieu Rodrigue",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/mlrequest/sklearn-json",
+     url="https://github.com/mignev/sklearn-json",
      packages=setuptools.find_packages(),
      install_requires=[
           'scikit-learn>=0.21.3',
